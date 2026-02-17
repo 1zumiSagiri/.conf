06eef4c924c2681e4eb1c97b5de7185790eb5b13
1zumiSagiri <iizumisagiri@icloud.com>
2026-02-17 11:10:30 -0500

    save

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -103,6 +103,15 @@ return {
         end
     },
 
+    {
+        "lervag/vimtex",
+        lazy = false,
+        init = function()
+            vim.g.vimtex_view_general_viewer = "okular"
+            vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+        end
+    },
+
     -- =====================
     -- Error diagnostics
     -- =====================
